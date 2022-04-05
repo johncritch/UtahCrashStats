ff1c311afe9493fa4a3945c38529f91233417fc5
johncritch <johnjohncritchcritch@gmail.com>
2022-04-05 10:38:11 -0600

    added search functionality

--- UtahCrashStats/Pages/Crashes.cshtml.cs
+++ UtahCrashStats/Pages/Crashes.cshtml.cs
@@ -4,23 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using UtahCrashStats.Models;
 
 namespace UtahCrashStats.Pages
 {
     public class CrashesModel : PageModel
     {
-        private readonly UtahCrashStats.Models.CrashDbContext _context;
+        private readonly UtahCrashStats.Models.CrashDbContext context;
 
-        public CrashesModel(UtahCrashStats.Models.CrashDbContext context)
+        public CrashesModel(UtahCrashStats.Models.CrashDbContext _context)
         {
-            _context = context;
+            context = _context;
         }
 
-        public IActionResult OnGetSelectAll()
+        public List<Crash> crashes { get; set; }
+
+        public int totalCrashes { get; set; }
+
+        public int pageNum { get; set; }
+
+        public int pageSize { get; set; }
+
+        public string searchTerm { get; set; }
+
+        public void OnGet(int p = 1, int s = 10, string search = "")
         {
-            List<Crash> data = _context.Crash.ToList();
-            return new JsonResult(data);
+            searchTerm = search;
+
+            crashes = context.Crash
+                .Where(x => x.CITY.Contains(search) ||
+                x.COUNTY_NAME.Contains(search) ||
+                x.MAIN_ROAD_NAME.Contains(search))
+                .Skip((p - 1) * s)
+                .Take(s)
+                .ToList();
+            totalCrashes = context.Crash
+                .Where(x => x.CITY.Contains(search) ||
+                x.COUNTY_NAME.Contains(search) ||
+                x.MAIN_ROAD_NAME.Contains(search))
+                .Count();
+            pageSize = s;
+            pageNum = p;
         }
+        //public async Task OnGetAsync(int p = 1, int s = 15)
+        //{
+        //    Crash = await _context.Crash
+        //        .Skip((p - 1) * s)
+        //        .Take(s)
+        //        .ToListAsync();
+        //    pageSize = s;
+        //    totalCrashes = Crash.Count;
+        //    pageNum = p;
+        //}
     }
 }
